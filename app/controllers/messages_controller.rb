1d1c8a614aded3ddf61abb1b41a0c86d75f7ccb1
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-07-03 21:16:30 +0900

    createMessageFunction

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,34 @@
 class MessagesController < ApplicationController
+  before_action :set_group
   def index
+    @message = Message.new
+    # @group.messagesの個々のメッセージのuserをincludesで取得
+    @messages = @group.messages.includes(:user)
+  end
+
+  def create
+    # @group.messages.new が今ひとつ..
+    # 保存に成功した場合と失敗した場合で分岐
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力して下さい'
+      render  :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
--- db/migrate/20190701135843_create_messages.rb
+++ db/migrate/20190701135843_create_messages.rb
@@ -1,7 +1,12 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.string  :content
+      t.string  :image
+      # 外部キー references型で設定するので、group_idのような_idは不要となる
+      # referencesを使用すると、インデックスの設定も自動的に行われる
+      t.references  :group, foreign_key: true
+      t.references  :user, foreign_key: true
       t.timestamps
     end
   end
