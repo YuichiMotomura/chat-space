0b233eb1be31392283b0221818dced15e3ddc8db
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-07-20 14:55:51 +0900

    after_refactoring#2

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update, :index]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-    # groups/:group_id/messages のように、グループ内のmessageが
-    # 分かるようにする
     namespace :api do
       resources :messages, only:  :index, defaults: { format: 'json' }
     end
