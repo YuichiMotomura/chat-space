601d4337736bab13f431af2de355834e8bd8fdcc
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-07-25 16:06:00 +0900

    Add_gem_fog

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,18 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/stroage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.stroage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory = 'yu1-photo'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yu1-photo'
+
+end
